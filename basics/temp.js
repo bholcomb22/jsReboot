1ec3d9fb7733b670d64f853a0e5dc0d44984f149
Ben Holcomb <bholcomb22@Bens-MacBook-Pro.local>
2019-06-09 15:20:09 -0600

    first commit

---
+++ basics/temp.js
@@ -0,0 +1,15 @@
+let fTemp = 32;
+let celcius = (fTemp - 32) * 5/9;
+let kelvin = (fTemp + 459.67) * 5/9;
+
+
+
+console.log('Farenheit: ' + fTemp + ', Celcius: ' + celcius + ', Kelvin: ' + kelvin);
+
+//challenge
+
+let age = 29;
+let isChild = age <= 12;
+let isSenior = age >= 65;
+console.log(isChild);
+console.log(isSenior);
