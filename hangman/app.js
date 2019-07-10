e88d38aa363c51294c64a396fac8588a5108ab55
Ben Holcomb <bholcomb22@yahoo.com>
2019-07-10 08:30:57 -0600

    updated functionality to hangman game

--- hangman/app.js
+++ hangman/app.js
@@ -1,17 +1,43 @@
 
+
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
-const game1 = new Hangman('developer', 5)
+const game1 = new Hangman('New Jersey', 5)
 
-puzzleEl.innerHTML = `Hangman: ${game1.getPuzzle()}`
-guessesEl.innerHTML = `Guesses left:${game1.remainingGuesses}`
-console.log(game1.status)
+puzzleEl.innerHTML = game1.puzzle
+guessesEl.innerHTML = `Guesses left: ${game1.statusMessage}`
 
-window.addEventListener('keypress', function(e){
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     game1.makeGuess(guess)
-    puzzleEl.textContent = game1.getPuzzle()
-    guessesEl.textContent = game1.remainingGuesses
-    console.log(game1.status)
+    puzzleEl.textContent = game1.puzzle
+    guessesEl.textContent = game1.statusMessage
+    
 })
 
+ getPuzzle((error, puzzle) => {
+     if(error) {
+         console.log(`Error: ${error}`)
+     }else {
+        console.log(puzzle)
+     }
+})
+
+
+
+// const request2 = new XMLHttpRequest()
+// request2.addEventListener('readystatechange', (e) => {
+//     if(e.target.readyState === 4 && e.target.status === 200){
+//         const data = JSON.parse(e.target.responseText)
+//         let answer = ''
+//         const myCountry = data.forEach((item) => {
+//             if (item.alpha2Code === "US")
+//             answer = ` Country Code: ${item.alpha2Code} Country:${item.name}`
+//         })
+//         console.log(answer)
+//     }
+// })
+
+// request2.open('GET', 'http://restcountries.eu/rest/v2/all')
+// request2.send()
--- oop/person.js
+++ oop/person.js
@@ -1,30 +1,59 @@
-const Person = function(first, last, age, likes = []) {
-    this.first = first
-    this.last = last
-    this.age = age
-    this.likes = likes
-}
-console.log()
 
-Person.prototype.getBio = function(){
-    let bio = `${this.first} ${this.last} is ${this.age} years old.`
+class Person {
+    constructor(first, last, age, likes = []) {
+        this.first = first
+        this.last = last
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.first} ${this.last} is ${this.age} years old.`
 
-    this.likes.forEach((like) => {
-        bio += ` ${this.first} likes ${like}.`
+        this.likes.forEach((like) => {
+            bio += ` ${this.first} likes ${like}.`
     })
 
     return bio
+    }
+     set fullName(fullName) {
+        let parsedFirst = fullName.split(' ')
+        this.first = parsedFirst[0]
+        this.last = parsedFirst[1]
+    }
+    get fullName(){
+        return `${this.first} ${this.last}`
+    }
 }
 
-Person.prototype.setName = function(fullName){
-    let parsedFirst = fullName.split(' ')
-    this.first = parsedFirst[0]
-    this.last = parsedFirst[1]
-    //console.log(this)
+class Employee extends Person {
+    constructor(first, last, age, position, likes){
+        super(first, last, age, likes)
+        this.position = position
+    }
+    getBio() {
+        return `${this.fullName} is ${this.position}`
+    }
+    getYearsLeft() {
+        return 65 - this.age
+    }
 }
-    
+
+class Student extends Person {
+    constructor(first, last, age, grade, likes){
+        super(first, last, age, likes)
+        this.grade = grade
+    }
+    getBio(){
+        const status = this.grade >= 70 ? 'passing' : 'failing' 
+        return `${this.first} ${this.last} is currently ${status}`
+    }
+    updateGrade(number){
+        this.grade += number
+    }
+}
+const me = new Employee('Ben', 'Holcomb', 29, 'developer')
+me.fullName = 'Amelia Holcomb'
+
+console.log(me.getBio())
 
 
-const amelia = new Person('Amelia', 'Holcomb', 30, ['cuddling', 'being cute'])
-
-console.log(amelia.getBio())
